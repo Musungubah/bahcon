83b4e08d1af2e73de84ac65120a3d461a7f3f004
user <user@this.com>
2019-11-06 15:28:49 -0500

    Fixed delete and create by name

---
+++ demo/src/main/java/com/bah/msd/controller/EventController.java
@@ -0,0 +1,65 @@
+package com.bah.msd.controller;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.bah.msd.model.Event;
+import com.bah.msd.repository.EventRepository;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+	
+	@Autowired
+	EventRepository repo;
+	
+	@GetMapping
+	public Iterable<Event> getAll(){
+		return repo.findAll();
+	}
+	
+	@GetMapping("/{EventId}")
+	public Optional<Event> getCustomerById(@PathVariable("customerId") long id){
+		return repo.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> addEvent(@RequestBody Event newEvent,
+			UriComponentsBuilder uri){
+		if(newEvent.getId() != 0 || newEvent.getName() == null 
+				|| newEvent.getDate() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		newEvent=repo.save(newEvent);
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newEvent.getId()).toUri();
+		
+		ResponseEntity<?> response=ResponseEntity.created(location).build();
+		return response;
+	}
+	
+	@PutMapping("/{EventId}")
+	public ResponseEntity<?> putEvent(@RequestBody Event newEvent, 
+			@PathVariable("customerId") long customerId){
+		if(newEvent.getId() != customerId|| newEvent.getName() == null 
+				|| newEvent. getDate()== null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newEvent = repo.save(newEvent);
+		return ResponseEntity.ok().build();
+	}
+	
+}
--- demo/src/main/java/com/bah/msd/model/Event.java
+++ demo/src/main/java/com/bah/msd/model/Event.java
@@ -7,30 +7,61 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-	
-	  @Entity
-	  
-	  @Table(name="EVENTS") public class Event {
-	  
-	  @Id
-	  
-	  @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
-	  
-	  @Column(name="EVENT_NAME") private String name; private String date;
-	  
-	  public Event() { super();} // TODO Auto-generated constructor stub }
-	  
-	  public Event(Long id, String name, String date) { super(); this.id = id;
-	  this.name = name; this.date = date; }
-	  
-	  public String getName() { return name; } public void setName(String name) {
-	  this.name = name; }
-	  
-	  public Long getId() { return id; }
-	  
-	  public void setId(Long id) { this.id = id; }
-	  
-	  public String getDate() { return date; }
-	  
-	  public void setDate(String date) { this.date = date; } }
-	 
+@Entity
+
+@Table(name = "EVENTS")
+public class Event {
+
+	@Id
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "EVENT_NAME")
+	private String name;
+	private String date;
+	private Integer customerId;
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public Event() {
+		super();
+	} // TODO Auto-generated constructor stub }
+
+	public Event(Long id, String name, String date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
